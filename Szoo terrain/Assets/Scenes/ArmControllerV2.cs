e5d58c71ad8dcb178bc496ed5c886c5930693a1d
Sune <sune171205@gmail.com>
2023-04-17 13:47:21 +0200

    Hånd.bevæg
    arm også :)

--- Szoo terrain/Assets/Scenes/ArmControllerV2.cs
+++ Szoo terrain/Assets/Scenes/ArmControllerV2.cs
@@ -3,31 +3,30 @@ using System.Collections;
 
 public class ArmControllerV2 : MonoBehaviour
 {
-    public float scaleSpeed = 1.0f;  // The speed at which the object will scale
-    public float scaleDownSpeed = 5.0f; // The speed at which the object will scale down
-
     private bool isScaling = false;  // A flag to check if the object is currently being scaled
     private float originalZScale;   // The original z scale of the object
 
     void Start()
     {
         originalZScale = transform.localScale.z;
+        Debug.Log(HandController.HandGoGetMilkSpeed);
     }
 
     void Update()
     {
+        
         // Check if the object is being scaled and scale it accordingly
         if (isScaling)
         {
             Vector3 newScale = transform.localScale;
-            newScale.z += scaleSpeed * Time.deltaTime;
+            newScale.z += HandController.HandGoGetMilkSpeed * Time.deltaTime;
             transform.localScale = newScale;
         }
         else
         {
             // If the object is not being scaled, scale it back to its original size
             Vector3 newScale = transform.localScale;
-            newScale.z -= scaleDownSpeed * Time.deltaTime;
+            newScale.z -= HandController.HandGotMilkSpeed * Time.deltaTime;
             newScale.z = Mathf.Max(newScale.z, originalZScale);
             transform.localScale = newScale;
         }
@@ -42,6 +41,8 @@ public class ArmControllerV2 : MonoBehaviour
             isScaling = false;
             Debug.Log("Mouse no pressed");
         }
+
+        
     }
 
 }
--- Szoo terrain/Assets/Scenes/HandController.cs
+++ Szoo terrain/Assets/Scenes/HandController.cs
@@ -3,20 +3,25 @@ using UnityEngine.EventSystems;
 
 public class HandController : MonoBehaviour
 {
-    public float HandGoGetMilkSpeed = 1.0f;
-    public float HandGotMilkSpeed = 5.0f; 
+    public static float HandGoGetMilkSpeed = 250f;
+    public static float HandGotMilkSpeed = 1000f; 
 
-    void Update()
-    {
+    private bool isMoving = false;
+    private float originalZScale;
+
         
-        if (Input.GetMouseButtonDown(0))
-        {
-            Debug.Log("Pressed Mouse 2");
+    public GameObject Wrist;
 
-        }
-        else if (Input.GetMouseButtonUp(0))
-        {
-            Debug.Log("Mouse no pressed 2");
-        }
+
+    void Start()
+    {
+        Debug.Log(HandGoGetMilkSpeed);
     }
+
+    public void Update(){
+
+        this.transform.position = Wrist.transform.position;
+    }
+
+    
 }
